d9c00c583f8100d9a99f90d75b51aac8b0a424a3
Kayla Matthews <kdmatthews77@gmail.com>
2024-07-10 16:25:33 -0400

    Code Clean Up

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,6 @@ function Footer() {
       <div className="footer-container">
         <img src="littlelemonlogo.jpg" alt="little lemon logo" />
         <p>©2024 copyright</p>
-     
       </div>
       </>
     );
